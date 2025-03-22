7160432acc718fc8157e9dcdf0bc38e51e24a887
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-22 21:49:43 +0530

    ATM defined for the app

--- src/models/ATM.java
+++ src/models/ATM.java
@@ -1,2 +1,47 @@
-package models;public class ATM {
+package models;
+
+import atmstates.ATMState;
+import enums.DenominationType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ATM {
+
+    public static volatile ATM atm;
+    private ATMState atmState;
+    private final Map<DenominationType, Integer> denominations;
+    private int amount;
+
+    private ATM(Map<DenominationType, Integer> denominations) {
+        this.atmState = new ATMState();
+        this.denominations = new HashMap<>();
+        addMoney(denominations);
+    }
+
+    public static ATM getInstance(Map<DenominationType, Integer> denominations) {
+        if(atm == null) {
+            synchronized (ATM.class) {
+                if(atm == null) {
+                    atm = new ATM(denominations);
+                }
+            }
+        }
+        return atm;
+    }
+
+    public void addMoney(Map<DenominationType, Integer> currencies) {
+        for(DenominationType currency : currencies.keySet()) {
+            this.denominations.put(currency, this.denominations.getOrDefault(currency, 0) + currencies.get(currency));
+            this.amount += (denominations.get(currency) * currency.getValue());
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAtmState(ATMState atmState) {
+        this.atmState = atmState;
+    }
 }
