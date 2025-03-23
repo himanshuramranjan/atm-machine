69a7d2a92912569b20e6112d16c0ec7aae4c1086
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-23 17:36:36 +0530

    cash withdrawal process added to the state

--- src/atmstates/CashWithdrawalState.java
+++ src/atmstates/CashWithdrawalState.java
@@ -2,6 +2,10 @@ package atmstates;
 
 import models.ATM;
 import models.Card;
+import withdrawal_processor.CashWithdrawalProcessor;
+import withdrawal_processor.FiveHundredCashWithdrawalProcessor;
+import withdrawal_processor.HundredCashWithdrawalProcessor;
+import withdrawal_processor.TwoThousandCashWithdrawalProcessor;
 
 public class CashWithdrawalState extends ATMState {
     private final ATM atm;
@@ -18,6 +22,13 @@ public class CashWithdrawalState extends ATMState {
         boolean isTransactionSuccessful = this.card.getBankAccount().deductAmount(amount);
         if(!isTransactionSuccessful) {
             System.out.println("Insufficient Account balance");
+        } else {
+            CashWithdrawalProcessor cashWithdrawalProcessor = new TwoThousandCashWithdrawalProcessor(new FiveHundredCashWithdrawalProcessor(new HundredCashWithdrawalProcessor(null)));
+            boolean cashWithdrawn = cashWithdrawalProcessor.withdrawCash(this.atm, amount);
+            if(!cashWithdrawn) {
+                this.card.getBankAccount().creditMoney(amount);
+                System.out.println("Unable to despense money");
+            }
         }
         exitTransaction();
     }
