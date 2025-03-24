8ba8b2af63ccd0b6ebc245d2c0ef61dae27c2062
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-24 21:44:24 +0530

    updated the models

--- src/models/ATM.java
+++ src/models/ATM.java
@@ -38,6 +38,10 @@ public class ATM {
         }
     }
 
+    public ATMState getAtmState() {
+        return atmState;
+    }
+
     public int getAmount() {
         return amount;
     }
--- src/models/BankAccount.java
+++ src/models/BankAccount.java
@@ -1,10 +1,13 @@
 package models;
 
 public class BankAccount {
+
+    private String customerName;
     private final String accountId;
     private double balance;
 
-    public BankAccount(String accountId, double balance) {
+    public BankAccount(String name, String accountId, double balance) {
+        this.customerName = name;
         this.accountId = accountId;
         this.balance = balance;
     }
